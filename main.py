9a70d9fa1ff4919848dbc6ac19e0123a69aff23e
rawan645 <rwan_a00645@cic-cairo.com>
2026-05-10 14:56:08 +0300

    Update model path handling in main.py
    Refactor model path to use os.path for better portability.

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ from PIL import Image
 import tensorflow as tf
 import numpy as np
 import io
+import os
 
 app = FastAPI()
 
-interpreter = tf.lite.Interpreter(model_path="best_float16.tflite")
+MODEL_PATH = os.path.join(os.path.dirname(__file__), "best_float16.tflite")
+interpreter = tf.lite.Interpreter(model_path=MODEL_PATH)
 interpreter.allocate_tensors()
 
 input_details = interpreter.get_input_details()
@@ -18,21 +20,12 @@ def home():
 
 @app.post("/predict")
 async def predict(file: UploadFile = File(...)):
-
     image_bytes = await file.read()
-
     image = Image.open(io.BytesIO(image_bytes)).resize((224,224))
-
     img = np.array(image).astype(np.float32)
     img = np.expand_dims(img, axis=0)
-
     interpreter.set_tensor(input_details[0]['index'], img)
     interpreter.invoke()
-
     output = interpreter.get_tensor(output_details[0]['index'])
-
     prediction = int(np.argmax(output))
-
-    return {
-        "prediction": prediction
-    }
+    return {"prediction": prediction}
